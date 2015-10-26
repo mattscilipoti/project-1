119328607c75c0a95a09100133587e9cec7c65b3
ANDRE SHONUBI <andre@codedre.com>
2015-10-26 08:53:04 -0400

    working on setting up my game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
+    var events = [];
+    var squaresMatch = false;
+
   //create gameboard
   $("#gameboard").append('<table><tr id="row1" class="row"><td></td><td></td></tr><tr id="row2" class="row"><td></td><td></td></tr></table>');
+
+  function checkForMatch(eventsArray){
+    if (eventsArray[0].css("background-color") === eventsArray[1].css("background-color")){
+      eventsArray[0].addClass('matching');
+      eventsArray[1].addClass('matching');
+      squaresMatch = !squaresMatch;
+    } else {
+      eventsArray[0].removeClass('matching');
+      eventsArray[1].removeClass('matching');
+    }
+  }
 
   //perform action on clicked square
   $("table").on("click", function(){
     var target = $(event.target);
     target.addClass('flipped');
-  });
+    events.push(target);
 
+    if (events.length === 2){
+      //check to see if they have a match
+      checkForMatch(events);
+      if (squaresMatch){
+        $("table").off();
+      }
+    }
+
+  });
 });
