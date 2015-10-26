e980a4e869edb9a2e9f67f6677569b44e0441ae3
ANDRE SHONUBI <andre@codedre.com>
2015-10-26 10:54:33 -0400

    add some image assets and got squares to change background to image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+    var menuPath = "images/menu/";
     var events = [];
     var squaresMatch = false;
+    var gameSquares = [menuPath + "Sandwich1.png", menuPath + "Burger2.png"];
 
   //create gameboard
   $("#gameboard").append('<table><tr id="row1" class="row"><td></td><td></td></tr><tr id="row2" class="row"><td></td><td></td></tr></table>');
@@ -8,26 +10,31 @@ $(document).ready(function() {
   function checkForMatch(eventsArray){
     if (eventsArray[0].css("background-color") === eventsArray[1].css("background-color")){
       eventsArray[0].addClass('matching');
-      eventsArray[1].addClass('matching');
+      eventsArray[1].addClass('matching').css('background-image', gameSquares[1]);
+
       squaresMatch = !squaresMatch;
     } else {
-      eventsArray[0].removeClass('matching');
-      eventsArray[1].removeClass('matching');
+      console.log("not matching");
+      eventsArray[0].removeClass('flipped');
+      eventsArray[1].removeClass('flipped');
     }
   }
 
   //perform action on clicked square
   $("table").on("click", function(){
     var target = $(event.target);
-    target.addClass('flipped');
+    var i=0;
+    target.addClass('flipped').css('background-image', "url(" + gameSquares[i] + ")");
+    i++;
     events.push(target);
 
     if (events.length === 2){
       //check to see if they have a match
-      checkForMatch(events);
-      if (squaresMatch){
-        $("table").off();
-      }
+      console.log("I have two evnets");
+      //checkForMatch(events);
+      // if (squaresMatch){
+      //   $("table").off();
+      // }
     }
 
   });
