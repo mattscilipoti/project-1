8c1304684a9180d5279041ae257922710ba9573b
ANDRE SHONUBI <andre@codedre.com>
2015-10-25 22:38:36 -0400

    playing around

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  console.log("we are all good here");
+  //create gameboard
+  $("#gameboard").append('<table><tr id="row1" class="row"><td></td><td></td></tr><tr id="row2" class="row"><td></td><td></td></tr></table>');
+
+  //perform action on clicked square
+  $("table").on("click", function(){
+    var target = $(event.target);
+    target.addClass('flipped');
+  });
+
 });
